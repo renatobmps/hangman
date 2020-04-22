a34896dbca4fba7d7d0b4da64ed3784f18839283
Renato <renatobmpsilva@gmail.com>
2020-04-22 03:27:44 -0300

    first commit

---
+++ js/dados.js
@@ -0,0 +1,178 @@
+palavras = [
+    {
+        palavra: "Renato",
+        dica: "Família",
+    },
+    {
+        palavra: "Fernanda",
+        dica: "Família",
+    },
+    {
+        palavra: "Nicolas",
+        dica: "Família",
+    },
+    {
+        palavra: "Natália",
+        dica: "Família",
+    },
+    {
+        palavra: "Felicia",
+        dica: "Animal",
+    },
+    {
+        palavra: "Peixe",
+        dica: "Animal",
+    },
+    {
+        palavra: "Azul",
+        dica: "Cor",
+    },
+    {
+        palavra: "Verde",
+        dica: "Cor",
+    },
+    {
+        palavra: "Amarelo",
+        dica: "Cor",
+    },
+    {
+        palavra: "Rosa",
+        dica: "Cor",
+    },
+    {
+        palavra: "Vermelho",
+        dica: "Cor",
+    },
+    {
+        palavra: "Branco",
+        dica: "Cor",
+    },
+    {
+        palavra: "Preto",
+        dica: "Cor",
+    },
+    {
+        palavra: "Camisa",
+        dica: "Vestuário",
+    },
+    {
+        palavra: "Saia",
+        dica: "Vestuário",
+    },
+    {
+        palavra: "Vestido",
+        dica: "Vestuário",
+    },
+    {
+        palavra: "Blusa",
+        dica: "Vestuário",
+    },
+    {
+        palavra: "Calça",
+        dica: "Vestuário",
+    },
+    {
+        palavra: "Bermuda",
+        dica: "Vestuário",
+    },
+    {
+        palavra: "Fada",
+        dica: "Personagem de contos",
+    },
+    {
+        palavra: "Bruxa",
+        dica: "Personagem de contos",
+    },
+    {
+        palavra: "Feiticeira",
+        dica: "Personagem de contos",
+    },
+    {
+        palavra: "Príncipe",
+        dica: "Personagem de contos",
+    },
+    {
+        palavra: "Princesa",
+        dica: "Personagem de contos",
+    },
+    {
+        palavra: "Rainha",
+        dica: "Personagem de contos",
+    },
+    {
+        palavra: "Arroz",
+        dica: "Alimento",
+    },
+    {
+        palavra: "Feijão",
+        dica: "Alimento",
+    },
+    {
+        palavra: "Açucar",
+        dica: "Alimento",
+    },
+    {
+        palavra: "Café",
+        dica: "Alimento",
+    },
+    {
+        palavra: "Pão",
+        dica: "Alimento",
+    },
+    {
+        palavra: "Leite",
+        dica: "Alimento",
+    },
+    {
+        palavra: "Andorinha",
+        dica: "Pássaro",
+    },
+    {
+        palavra: "Gaivota",
+        dica: "Pássaro",
+    },
+    {
+        palavra: "Arara",
+        dica: "Pássaro",
+    },
+    {
+        palavra: "Urubu",
+        dica: "Pássaro",
+    },
+    {
+        palavra: "Gavião",
+        dica: "Pássaro",
+    },
+    {
+        palavra: "Pardal",
+        dica: "Pássaro",
+    },
+    {
+        palavra: "Brasil",
+        dica: "País",
+    },
+    {
+        palavra: "Alemanha",
+        dica: "País",
+    },
+    {
+        palavra: "China",
+        dica: "País",
+    },
+    {
+        palavra: "Portugal",
+        dica: "País",
+    },
+    {
+        palavra: "México",
+        dica: "País",
+    },
+    {
+        palavra: "Argentina",
+        dica: "País",
+    },
+    {
+        palavra: "Uruguai",
+        dica: "País",
+    },
+]
---
+++ js/view.js
@@ -0,0 +1,78 @@
+listaBody = []
+
+//Seção pontuação
+
+listaBody.push(`
+<h1>Jogo da Forca</h1>
+<section class="secao-pontos">
+<p>Pontos: <span class="pontos">0</span></p>
+</section>
+`)
+
+function atualizaPontuacao(pontuacao) {
+    document.querySelector(".pontos").innerText = pontuacao
+
+    if(pontuacao < 1){
+        alert("Perdeu!")
+        document.querySelectorAll(".alfabeto button").forEach(button => {
+            button.setAttribute("disabled", true)
+        })
+        document.querySelectorAll(".escondida").forEach(letra => {
+            letra.style = "color: red; font-weight: bold;"
+        })
+        document.querySelector(".palavra").style = "color: red;"
+        setTimeout(function(){
+            alert("Recarregando para uma nova partida")
+            document.location.reload(true);
+        }, 10000)
+    }
+}
+
+
+//Seção de palavra sorteada
+listaPalavra = []
+
+palavra.forEach(letra => {
+    listaPalavra.push(`<p class="letra-${removeAcento(letra)} escondida">${letra}</p>`)
+})
+
+function removeAcento (text)
+{       
+    text = text.toLowerCase();                                                         
+    text = text.replace(new RegExp('[ÁÀÂÃ]','gi'), 'a');
+    text = text.replace(new RegExp('[ÉÈÊ]','gi'), 'e');
+    text = text.replace(new RegExp('[ÍÌÎ]','gi'), 'i');
+    text = text.replace(new RegExp('[ÓÒÔÕ]','gi'), 'o');
+    text = text.replace(new RegExp('[ÚÙÛ]','gi'), 'u');
+    text = text.replace(new RegExp('[Ç]','gi'), 'c');
+    return text;                 
+}
+
+listaBody.push(`
+<section class="secao-palavra">
+<div class="palavra">
+${listaPalavra.join("")}
+</div>
+<p>Dica: ${dica}</p>
+</section>
+`)
+
+//Seção de letras selecionáveis
+listaAlfabeto = []
+
+alfabeto = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s","t","u","v","w","x","y","z"]
+
+alfabeto.forEach(letra => {
+    listaAlfabeto.push(`<button id="letra-${letra}" value="${letra}">${letra}</button>`)
+});
+
+listaBody.push(`
+<section class="secao-alfabeto">
+<div class="alfabeto">
+${listaAlfabeto.join("")}
+</div>
+<p class="erros"></p>
+</section>    
+`)
+
+document.body.innerHTML = listaBody.join("")
