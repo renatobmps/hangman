61d03f157717d78aa489873f3f2f4f8044f37740
Renato Brandão <renatobmps.dev@gmail.com>
2022-07-28 03:43:48 -0300

    change to api

---
+++ js/login.js
@@ -0,0 +1,47 @@
+function postLogin(event) {
+  event.preventDefault();
+
+  const data = {
+    user: document.getElementById('login').value,
+    password: document.getElementById('password').value
+  };
+
+  if (!data.user || data.user.length < 3) {
+    alert('Problema com o login');
+    return document.getElementById('login').focus();
+  }
+
+  if (!data.password || data.password.length < 3) {
+    alert('Problema com a senha');
+    return document.getElementById('password').focus();
+  }
+
+  fetch('http://localhost:3000/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  }).then(async response => {
+    const body = await response.json();
+
+    if (body.error) throw new Error(body.error);
+
+    const { token } = body;
+
+    localStorage.setItem('token', token);
+
+    window.location.href = './index.html';
+  }).catch(error => {
+    console.log(error);
+    alert(error || 'Problema com o login');
+  });
+}
+
+(() => {
+  const token = localStorage.getItem('token');
+
+  if (token) {
+    window.location.href = './index.html';
+  }
+})()
---
+++ js/sigin.js
@@ -0,0 +1,71 @@
+function postSign(event) {
+  event.preventDefault();
+
+  const data = {
+    name: document.getElementById('login').value,
+    password: document.getElementById('password').value,
+    confirmPassword: document.getElementById('confirm_password').value,
+  };
+
+  if (!data.name || data.name.length < 3) {
+    alert('Problema com o login');
+    return document.getElementById('login').focus();
+  }
+
+  if (!data.password || data.password.length < 3) {
+    alert('Problema com a senha');
+    return document.getElementById('password').focus();
+  }
+
+  if (data.password !== data.confirmPassword) {
+    alert("Senhas não conferem");
+    return document.getElementById("confirm_password").focus();
+  }
+
+  fetch('http://localhost:3000/users/', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  }).then(async response => {
+    const body = await response.json();
+
+    if (body.error) throw new Error(body.error);
+
+    fetch('http://localhost:3000/login/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        user: data.name,
+        password: data.password
+      })
+    }).then(async response => {
+      const body = await response.json();
+
+      if (body.error) throw new Error(body.error);
+
+      const { token } = body;
+
+      localStorage.setItem('token', token);
+
+      window.location.href = './index.html';
+    }).catch(error => {
+      console.error(error);
+      alert(error || 'Problema com o login');
+    });
+  }).catch(error => {
+    console.error(error);
+    alert(error || 'Problema com o login');
+  });
+}
+
+(() => {
+  const token = localStorage.getItem('token');
+
+  if (token) {
+    window.location.href = './index.html';
+  }
+})()
