0f83ccf14cd172c44a60f0537bb666db7e2b48d0
Renato Brandão <renatobmpsilva@gmail.com>
2024-01-12 16:23:51 -0300

    status api data

--- src/pages/api/v1/status.tsx
+++ src/pages/api/v1/status.tsx
@@ -1,7 +1,15 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
-export default function handle(_: NextApiRequest, res: NextApiResponse) {
-  return res.json({
-    status: "ok",
+export default async function handle(_: NextApiRequest, res: NextApiResponse) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(
+        res.json({
+          status: "ok",
+          time_stamp: Date.now(),
+          random: Math.floor(Math.random() * 1024),
+        })
+      );
+    }, 2000);
   });
 }
