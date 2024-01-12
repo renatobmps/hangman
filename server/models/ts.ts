0057cfe3f8106ea11a5bd84df7b496820c5b698c
Renato Brandão <renatobmpsilva@gmail.com>
2024-01-12 19:01:45 -0300

    debugging server

--- server/models/ts.ts
+++ server/models/ts.ts
@@ -1,3 +1,6 @@
+import Sequelize, { DataTypes } from "sequelize";
+import TriedLetters from "./triedletters";
+
 export default {
     TriedLetters: {
         findAndCountAll: async () => ({ count: 15 })
