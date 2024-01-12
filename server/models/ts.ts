aeebcf8c83b59c6a29a85eed297e4bb1c62a276c
Renato Brandão <renatobmpsilva@gmail.com>
2024-01-12 19:19:28 -0300

    debugging server

--- server/models/ts.ts
+++ server/models/ts.ts
@@ -14,7 +14,7 @@ export default {
                 }
             );
 
-            TriedLetters(sequelize, DataTypes);
+            // TriedLetters(sequelize, DataTypes);
 
             return { count: 15 }
         }
