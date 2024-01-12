54fb104e6a74f7025e59b296cf935d454a289c0d
Renato Brandão <renatobmpsilva@gmail.com>
2024-01-12 18:14:25 -0300

    testing structure

--- src/pages/api/v1/login/index.ts
+++ src/pages/api/v1/login/index.ts
@@ -1,6 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import login from "../../../../server/controllers/Login";
-import ApiError from '../@types/ApiError';
+import login from "../../../../../server/controllers/Login";
+import ApiError from '../../@types/ApiError';
 
 type Method = 'POST';
 
--- src/pages/api/v1/status/index.tsx
+++ src/pages/api/v1/status/index.tsx
@@ -1,15 +1,47 @@
-import { NextApiRequest, NextApiResponse } from "next";
+import type { NextApiRequest, NextApiResponse } from "next";
+import login from "../../../../../server/controllers/Login";
+import ApiError from "../../@types/ApiError";
 
-export default async function handle(_: NextApiRequest, res: NextApiResponse) {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(
-        res.json({
-          status: "ok",
-          time_stamp: Date.now(),
-          random: Math.floor(Math.random() * 1024),
-        })
-      );
-    }, 2000);
-  });
+type Method = "GET";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { method } = req;
+
+  const methods: Record<
+    Method,
+    (req: NextApiRequest, res: NextApiResponse) => Promise<void>
+  > = {
+    GET: () =>
+      new Promise((resolve) => {
+        setTimeout(() => {
+          resolve(
+            res.json({
+              status: "ok",
+              time_stamp: Date.now(),
+              random: Math.floor(Math.random() * 1024),
+            })
+          );
+        }, 2000);
+      }),
+  };
+
+  try {
+    const chosen = methods[method as Method];
+
+    if (!chosen) {
+      throw new ApiError(`Method ${method} not allowed`, 405);
+    }
+
+    await methods[method as Method](req, res);
+  } catch (err) {
+    const error = err as ApiError;
+    const cause = error.message ?? error.status ?? error ?? "Unexpected error";
+
+    res.status(error.status).json({ status: "ko", message: cause });
+
+    throw new ApiError(cause);
+  }
 }
