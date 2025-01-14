1f73b387f92197c5e57770f27b5ba9339da6c5e4
Renato Brandão <renatobmpsilva@gmail.com>
2025-01-14 03:36:40 -0300

    fix(dashboard): menu

--- frontend/src/app/dashboard/hints/components/sub_components/top.tsx
+++ frontend/src/app/dashboard/hints/components/sub_components/top.tsx
@@ -1,19 +1,65 @@
+'use client'
 import Image from 'next/image';
 import Link from 'next/link';
 import '../style.css';
 import LogoMenu from '../media/logo_menu.svg';
-import Caret from '../media/caret-down.svg';
+import { useEffect, useRef } from 'react';
 
-const Top = () => (
-  <header className='hints__top'>
-    <Link href='/'>
-      <Image alt='Hangman logo' height={LogoMenu.height} priority src={LogoMenu.src} width={LogoMenu.width} />
-    </Link>
-    <button>
-      <span>Menu</span>{` `}
-      <Image alt='menu' height={Caret.height} src={Caret.src} width={Caret.width} />
-    </button>
-  </header>
-)
+const Top = () => {
+  const details = useRef<HTMLDetailsElement>(null);
+
+  const closeDetails = () => {
+    if (details.current) {
+      details.current.open = false;
+    }
+  }
+
+  useEffect(() => {
+    const handleClick = (event: MouseEvent) => {
+      if (!details.current.contains(event.target as Node)) {
+        closeDetails();
+      }
+    };
+
+    const handleEsc = ({
+      key,
+      altKey,
+      ctrlKey,
+      metaKey,
+      shiftKey
+    }: KeyboardEvent) => {
+      if (key === 'Escape' && !altKey && !ctrlKey && !metaKey && !shiftKey) {
+        closeDetails();
+      }
+    }
+
+    if (details.current) {
+      window.addEventListener('click', handleClick);
+      window.addEventListener('keydown', handleEsc);
+    }
+
+    return () => {
+      document.removeEventListener("click", handleClick);
+      window.removeEventListener('keydown', handleEsc);
+    };
+  }, [details.current]);
+
+  return (
+    <header className='hints__top'>
+      <Link href='/'>
+        <Image alt='Hangman logo' height={LogoMenu.height} priority src={LogoMenu.src} width={LogoMenu.width} />
+      </Link>
+      <details open={false} ref={details}>
+        <summary>Menu</summary>
+        <nav>
+          <ul>
+            <li><Link href="/dashboard/hints" onClick={closeDetails}>Hints</Link></li>
+            <li><Link href="/" onClick={closeDetails}>Back to game</Link></li>
+          </ul>
+        </nav>
+      </details>
+    </header>
+  )
+}
 
 export default Top;
