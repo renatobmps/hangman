7508c3f7c13162fec3f82510f36a3c6674d412a1
Renato Brandão <renatobmpsilva@gmail.com>
2025-01-14 04:37:26 -0300

    fix(hints): improving find by name

--- frontend/src/app/dashboard/hints/components/sub_components/navigation.tsx
+++ frontend/src/app/dashboard/hints/components/sub_components/navigation.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { ChangeEvent } from 'react';
+import { ChangeEvent, useEffect, useRef } from 'react';
 import Image from 'next/image';
 import '../style.css';
 import Filter from '../media/filter.svg';
@@ -8,6 +8,7 @@ import { useRouter } from 'next/navigation';
 
 const Navigation = () => {
   const router = useRouter();
+  const searchRef = useRef(null);
 
   const handleSearch = ({ target }: ChangeEvent<HTMLInputElement>) => {
     const { value } = target;
@@ -15,10 +16,24 @@ const Navigation = () => {
     router.replace(value ? `?search=${value}` : '?');
   }
 
+  useEffect(() => {
+    const handleKeydown = ({ key, altKey, ctrlKey, metaKey, shiftKey }: KeyboardEvent) => {
+      if (key === 'k' && !altKey && !ctrlKey && !metaKey && !shiftKey) {
+        !!searchRef.current && searchRef.current.focus();
+      }
+    };
+
+    window.addEventListener('keydown', handleKeydown);
+
+    return () => {
+      window.removeEventListener('keydown', handleKeydown);
+    }
+  }, [])
+
   return (
     <nav className='hints__navigation'>
       <label className='hints__navigation-search'>
-        <input type="search" placeholder='Search' onChange={handleSearch} />
+        <input type="search" placeholder='Search (press K to search)' onChange={handleSearch} ref={searchRef} />
         <Image alt="filter" height={Search.height} src={Search.src} width={Search.width} />
       </label>
       <Image alt="filter" height={Filter.height} src={Filter.src} width={Filter.width} />
